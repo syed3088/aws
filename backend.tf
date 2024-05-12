7979cca87120b8a9a989c142badb7057100f1ab3
Syed Khadar <69810725+syed3088@users.noreply.github.com>
2024-05-12 23:44:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-3088-tf-state" # REPLACE WITH YOUR BUCKET NAME
+    bucket         = "devops-3088-tf-state1" # REPLACE WITH YOUR BUCKET NAME
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state-locking"
